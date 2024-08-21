86de3631c0ffc1215251d45fb9631c0086859e60
moises <moises.aguilar.et7@gmail.com>
2024-08-21 19:32:17 -0300

    creacion de nuevos metodos de control

--- controllers/tareas.controlador.ts
+++ controllers/tareas.controlador.ts
@@ -24,7 +24,71 @@ export const tareasControladoras = {
             })
         }
         
+    },
+
+
+    create : async (req : Request  , res : Response ) => {
+
+        try {
+            
+       
+            
+            const data = await servicioTarea.create(req.body);
+            //todos los datos q estamos recopilando en data vamos a restornarlos en formato json
+            return res.json(data);
+            
+        } catch (error:any) {
+            res.status(400).json({
+    
+                message : error.message
+            })
+        }
+        
+    },
+
+
+    update : async (req : Request  , res : Response ) => {
+
+        try {
+            //.....
+            const {id} = req.params;
+            
+            //llamaos al metodo update para poder actulizar
+            const data = await servicioTarea.update(id, req.body);
+            //todos los datos q estamos recopilando en data vamos a restornarlos en formato json
+            return res.json(data);
+            
+        } catch (error:any) {
+            res.status(400).json({
+    
+                message : error.message
+            })
+        }
+        
+    },
+
+    delete : async (req : Request  , res : Response ) => {
+
+        try {
+            //.....
+            const {id} = req.params;
+            
+           
+            const data = await servicioTarea.delete(id);
+            //todos los datos q estamos recopilando en data vamos a restornarlos en formato json
+            return res.json(data);
+            
+        } catch (error:any) {
+            res.status(400).json({
+    
+                message : error.message
+            })
+        }
+        
     }
+        
+
+
 
 
 }
--- service/tarea.service.ts
+++ service/tarea.service.ts
@@ -4,10 +4,26 @@ import { TareaModel } from "../model"
 
 export const servicioTarea = {
 
-    async getAll(){
+    getAll : async () => {
 
        return await TareaModel.find();
 
-    }
+    },
 
+    // con este atributo creamos y guardamos un nuevo documento en nuestra bd de mongo
+    create : async (entidad : object) => {
+        return await TareaModel.create(entidad);
+    },
+
+    //con este metodo actualizamos la base de datos con datos nuevos q queremos ingresar
+    update : async (id: string , body : object ) => {
+
+        return await TareaModel.findByIdAndUpdate(id , body);
+    },
+
+    //con este metodo eliminamos un documento con su respectivo id
+    delete : async (id:string) => {
+        return await TareaModel.findByIdAndDelete
+        (id);
+    }
 }
